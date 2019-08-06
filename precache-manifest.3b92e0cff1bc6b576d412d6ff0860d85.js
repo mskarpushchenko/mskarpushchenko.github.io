1e917f0eb7a6c5cc27c2244a4caecd78fb130b66
mskarpushchenko <mskarpushchenko@mail.ru>
2019-08-06 14:43:40 +0300

    initial commit

---
+++ precache-manifest.3b92e0cff1bc6b576d412d6ff0860d85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62275ca35930367edce532ea8c07c71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "7d14a79195c71e8ed613",
+    "url": "/static/css/main.810feb90.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "7d14a79195c71e8ed613",
+    "url": "/static/js/main.13752093.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
